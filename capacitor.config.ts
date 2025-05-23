8e6687e67d869de1e36441d4a98e5671b71c1ff7
salheb22 <arthursalheb10@gmail.com>
2025-05-23 09:37:40 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'sigfas-mobile-teste',
+  webDir: 'dist'
+};
+
+export default config;
